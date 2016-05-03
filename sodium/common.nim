e32b6ca28b4bddb7bbddb56de89513d8d94fa2a2
Michał Zieliński <michal@zielinscy.org.pl>
2016-05-03 17:15:12 +0200

    compat with collections

--- sodium/common.nim
+++ sodium/common.nim
@@ -6,14 +6,7 @@ template checkzero*(err: int) =
   if err != 0:
     raise newException(SodiumError, "libsodium error")
 
-proc byteArray*(data: string, size: static[int]): array[size, byte] =
-  if data.len != size:
-    raise newException(ValueError, "bad length")
-  copyMem(addr result, data.cstring, size)
-
-proc toBinaryString*[T: array](s: T): string =
-  const size = s.high - s.low + 1
-  result = newString(size)
-  copyMem(result.cstring, unsafeAddr(s), size)
+import collections/bytes
+export byteArray, toBinaryString
 
 {.passl:"-lsodium".}
